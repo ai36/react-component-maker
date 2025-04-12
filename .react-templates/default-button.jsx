bd63ec0128f7c7c84bba7825c0b1655e5491902d
Andrei Fedorov <4363626@gmail.com>
2025-04-12 12:33:54 -0700

    Initial version

---
+++ .react-templates/default-button.jsx
@@ -0,0 +1,9 @@
+import styles from "./${styleFileName}";
+
+export function ${componentName}() {
+    return (
+        <>
+            <button className="${styles.className}">Click me!</button>;
+        </>
+    );
+}
